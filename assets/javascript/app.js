15e1c5f5875062e73bda0368cf4f2eadc42c5397
Michael Grill <Ultramar@Michaels-MacBook-Pro.local>
2019-03-14 13:45:59 -0400

    dakka

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,6 +6,35 @@ var game={
     incorrect: 0,
     counter: 120,
 
+    countdown: function() {
+        game.counter--;
+        $("#counter-number").html(game.counter);
+        if (game.counter === 0) {
+          console.log("TIME UP");
+          game.done();
+        }
+    },
+
+    start: function() {
+        timer = setInterval(game.countdown, 1000);
+    
+        $("#sub-wrapper").prepend(
+          "<h2>Time Remaining: <span id='counter-number'>120</span> Seconds</h2>"
+        );
+    
+        $("#start").remove();
+    
+        for (var i = 0; i < questions.length; i++) {
+          card.append("<h2>" + questions[i].question + "</h2>");
+          for (var j = 0; j < questions[i].answers.length; j++) {
+            card.append("<input type='radio' name='question-" + i +
+              "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
+          }
+        }
+    
+        card.append("<button id='done'>Done</button>");
+    },
+
     
 }
 
